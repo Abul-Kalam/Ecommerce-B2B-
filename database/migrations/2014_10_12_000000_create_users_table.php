fe9ccd6ff9703f1b378384e6a9251b586842cb65
Shahriar Rahaman <zxsr15@gmail.com>
2018-08-28 14:42:03 +0600

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->json('localization')->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
--- routes/backend.php
+++ routes/backend.php
@@ -1,19 +1,24 @@
 <?php
 
-Route::get('dashboard', function () {
-    return view('backend.pages.dashboard');
-})->name('dashboard');
 
-Route::resource('users', 'UserController');
-Route::resource('tags', 'TagController');
-Route::resource('categories', 'CategoryController');
-Route::resource('invoices', 'InvoiceController');
-Route::resource('products', 'ProductController');
-Route::resource('media', 'MediaController');
-Route::resource('brands', 'BrandController');
-Route::resource('shops', 'ShopController');
-Route::resource('comments', 'CommentController');
-Route::resource('orders', 'OrderController');
-Route::resource('ratings', 'RatingController');
-Route::resource('offers', 'OfferController');
-Route::resource('futureproducts', 'FutureProductController');
+Route::middleware('auth')->group(function () {
+
+    Route::get('dashboard', function () {
+        return view('backend.pages.dashboard');
+    })->name('dashboard');
+
+    Route::resource('users', 'UserController');
+    Route::resource('tags', 'TagController');
+    Route::resource('categories', 'CategoryController');
+    Route::resource('invoices', 'InvoiceController');
+    Route::resource('products', 'ProductController');
+    Route::resource('media', 'MediaController');
+    Route::resource('brands', 'BrandController');
+    Route::resource('shops', 'ShopController');
+    Route::resource('comments', 'CommentController');
+    Route::resource('orders', 'OrderController');
+    Route::resource('ratings', 'RatingController');
+    Route::resource('offers', 'OfferController');
+    Route::resource('futureproducts', 'FutureProductController');
+
+    });
