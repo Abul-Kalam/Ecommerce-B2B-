28bcba892da38bced7300827b8ddf776d8ed43bd
Leemon Chandra Prodeep <leemonchandra@gmail.com>
2018-08-29 13:58:14 +0600

    Media design

--- config/nav.php
+++ config/nav.php
@@ -10,10 +10,17 @@ return [
             'icon_class'  => 'fa fa-dashboard',
         ],
         [
+            'type' => 'single',
+            'active' => false,
+            'label' => 'Media',
+            'route' => 'backend.dashboard',
+            'icon_class'  => 'fa fa-folder-open',
+        ],
+        [
             'type' => 'menu',
             'active' => false,
             'label' => 'Product Category',
-            'icon_class'  => 'fa fa-dashboard',
+            'icon_class'  => 'fa fa-list',
             'childs' => [
                 [
                     'label' => 'Add New',
