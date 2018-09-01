1047261c7ef94e5b0e4b2d9332f9e9907d548f54
Sawjal Bhuiyan <msawjal@gmail.com>
2018-09-01 16:48:45 +0600

    shop create re-design to be continue

--- resources/views/backend/pages/shop-create.blade.php
+++ resources/views/backend/pages/shop-create.blade.php
@@ -33,60 +33,148 @@
                             <div class="box-header with-border">
                                 <a href="#" class="btn btn-sm btn-default pull-right">All</a>
                             </div>
-                            <div class="box-body">
-                                <div class="row">
-                                    <div class="col-lg-6 form-group">
-                                        <label for="display_name_en">Display Name En</label>
-                                        <input type="text" class="form-control" id="display_name_en" name="display-name-en">
+                            <div class="box-body p-0">
+                                <div class="nav-tabs-custom mb-0">
+                                    <ul class="nav nav-tabs">
+                                        <li class=""><a href="#tab_1" data-toggle="tab" aria-expanded="false">Basic</a></li>
+                                        <li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="false">Address</a></li>
+                                        <li class="active"><a href="#tab_3" data-toggle="tab" aria-expanded="true">Others</a></li>
+                                        <li class="dropdown">
+                                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+                                            Dropdown <span class="caret"></span>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
+                                            <li role="presentation" class="divider"></li>
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
+                                        </ul>
+                                        </li>
+                                        <li class="pull-right">
+                                            <a href="#" class="text-muted"><i class="fas fa-cog"></i></a>
+                                        </li>
+                                    </ul>
+                                    <div class="tab-content">
+                                        <div class="tab-pane" id="tab_1">
+                                            <div class="row">
+                                                <div class="col-lg-6 form-group">
+                                                    <label for="display_name_en">Display Name En</label>
+                                                    <input type="text" class="form-control" id="display_name_en" name="display-name-en">
+                                                </div>
+                                                <div class="col-lg-6 form-group">
+                                                    <label for="display-name-bn">Display Name Bn</label>
+                                                    <input type="text" class="form-control" id="display-name-bn" name="display-name-bn">
+                                                </div>
+                                            </div>
+            
+                                            <div class="row">
+                                                <div class="col-lg-6 form-group">
+                                                    <label for="slug">Slug</label>
+                                                    <input type="text" class="form-control" id="slug" name="slug">
+                                                </div>
+                                                <div class="col-lg-6 form-group">
+                                                    <label for="display-name-bn">Status</label>
+                                                    <select class="form-control" placeholder="Select Status">
+                                                        <option>Active</option>
+                                                        <option>Inactive</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+            
+                                            <div class="form-group">
+                                                <label for="description">Description</label>
+                                                <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+                                            </div>
+            
+                                            <div class="form-group">
+                                                <label for="meta-title">Meta Title</label>
+                                                <input type="text" class="form-control" id="meta-title" name="meta-title">
+                                            </div>
+                                            
+                                            <div class="form-group">
+                                                <label for="meta-keywords">Meta Keywords</label>
+                                                <input type="text" class="form-control" id="meta-keywords" name="meta-keywords">
+                                            </div>
+            
+                                            <div class="form-group">
+                                                <label for="meta-description">Meta Description</label>
+                                                <textarea class="form-control" id="description" rows="3" name="meta-description"></textarea>
+                                            </div>
+                                                    
+                                            <div class="form-group">
+                                                <label for="feature-image-url">Feature Image Url</label>
+                                                <input type="text" class="form-control" id="feature-image-url" name="feature-image-url">
+                                            </div>
+                                        </div>
+                                        <!-- /.tab-pane -->
+                                        <div class="tab-pane" id="tab_2">
+                                            <div class="row">
+                                                <div class="col-lg-8">
+                                                    <div class="form-group">
+                                                        <label for="full-name">Full Name</label>
+                                                        <input type="text" class="form-control" id="full-name" name="full-name">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="address-line-1">Address Line 1</label>
+                                                <input type="text" class="form-control" id="address-line-1" name="address-line-1">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="address-line-2">Address Line 2</label>
+                                                <input type="text" class="form-control" id="address-line-2" name="address-line-2">
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-lg-6">
+                                                    <div class="form-group">
+                                                        <label for="city">City / Town</label>
+                                                        <input type="text" class="form-control" id="city" name="city">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <div class="form-group">
+                                                        <label for="state">State / Province / Region</label>
+                                                        <input type="text" class="form-control" id="state" name="state">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-lg-6">
+                                                    <div class="form-group">
+                                                        <label for="district">District</label>
+                                                        <input type="text" class="form-control" id="district" name="district">
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-6">
+                                                    <div class="form-group">
+                                                        <label for="division">Division</label>
+                                                        <input type="text" class="form-control" id="division" name="division">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-lg-6">
+                                                    <div class="form-group">
+                                                        <label for="zip">Zip / Postal Code</label>
+                                                        <input type="text" class="form-control" id="zip" name="zip">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- /.tab-pane -->
+                                        <div class="tab-pane active" id="tab_3">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                        It has survived not only five centuries, but also the leap into electronic typesetting,
+                                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                                        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                                        like Aldus PageMaker including versions of Lorem Ipsum.
+                                        </div>
+                                        <!-- /.tab-pane -->
                                     </div>
-                                    <div class="col-lg-6 form-group">
-                                        <label for="display-name-bn">Display Name Bn</label>
-                                        <input type="text" class="form-control" id="display-name-bn" name="display-name-bn">
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-lg-6 form-group">
-                                        <label for="slug">Slug</label>
-                                        <input type="text" class="form-control" id="slug" name="slug">
-                                    </div>
-                                    <div class="col-lg-6 form-group">
-                                        <label for="display-name-bn">Status</label>
-                                        <select class="form-control" placeholder="Select Status">
-                                            <option>Active</option>
-                                            <option>Inactive</option>
-                                        </select>
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="description">Address</label>
-                                    <textarea class="form-control" id="address" rows="3" name="address"></textarea>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="description">Description</label>
-                                    <textarea class="form-control" id="description" rows="3" name="description"></textarea>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="meta-title">Meta Title</label>
-                                    <input type="text" class="form-control" id="meta-title" name="meta-title">
-                                </div>
-                                
-                                <div class="form-group">
-                                    <label for="meta-keywords">Meta Keywords</label>
-                                    <input type="text" class="form-control" id="meta-keywords" name="meta-keywords">
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="meta-description">Meta Description</label>
-                                    <textarea class="form-control" id="description" rows="3" name="meta-description"></textarea>
-                                </div>
-                                        
-                                <div class="form-group">
-                                    <label for="feature-image-url">Feature Image Url</label>
-                                    <input type="text" class="form-control" id="feature-image-url" name="feature-image-url">
+                                    <!-- /.tab-content -->
                                 </div>
                             </div>
                             <div class="box-footer">
