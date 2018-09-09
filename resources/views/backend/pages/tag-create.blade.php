22a535fe9ae50e812880a6522eaca1ae66e7522d
Sawjal Bhuiyan <msawjal@gmail.com>
2018-09-09 15:25:54 +0600

    tag create page some update

--- resources/views/backend/pages/tag-create.blade.php
+++ resources/views/backend/pages/tag-create.blade.php
@@ -97,6 +97,80 @@
                                 <button type="submit" class="btn btn-primary pull-right">Submit</button>
                             </div>
                         </div>
+
+
+                        <div class="box box-solid mt-3">
+                            {{-- <div class="box-header with-border">
+                                <a href="#" class="btn btn-sm btn-default pull-right">All</a>
+                            </div> --}}
+                            <div class="box-body p-0">
+                                <div class="nav-tabs-custom mb-0">
+                                    <ul class="nav nav-tabs">
+                                        <li class="active"><a href="#tab_1" data-toggle="tab" aria-expanded="false">Basic</a></li>
+                                        <li class=""><a href="#tab_2" data-toggle="tab" aria-expanded="true">Meta</a></li>
+                                        <li class="pull-right">
+                                            <a href="#" class="text-muted"><i class="fas fa-cog"></i></a>
+                                        </li>
+                                    </ul>
+                                    <div class="tab-content">
+                                        <div class="tab-pane active" id="tab_1">
+                                            <div class="row">
+                                                <div class="col-lg-6 form-group {{  $errors->has('display-name-en') ? 'has-error' : '' }}">
+                                                    <label for="display_name_en">Display Name En</label>
+                                                    <input type="text" class="form-control" id="display-name-en" name="display-name-en">
+                                                    @if($errors->has('display-name-en'))
+                                                    <span class="help-block">The Display Name En field is required.</span>
+                                                    @endif
+                                                </div>
+                                                <div class="col-lg-6 form-group {{ $errors->has('display-name-bn') ? 'has-error' : '' }}">
+                                                    <label for="display-name-bn">Display Name Bn</label>
+                                                    <input type="text" class="form-control" id="display-name-bn" name="display-name-bn">
+                                                    @if($errors->has('display-name-bn'))
+                                                    <span class="help-block">The Display Name Bn field is required.</span>
+                                                    @endif
+                                                </div>
+                                            </div>
+            
+                                            <div class="row">
+                                                <div class="col-lg-12 form-group {{ $errors->has('slug') ? 'has-error' : '' }}">
+                                                    <label for="slug">Slug</label>
+                                                    <input type="text" class="form-control" id="slug" name="slug">
+                                                    @if($errors->has('slug'))<span id="helpBlock2" class="help-block">The slug field is required.</span>
+                                                    @endif
+                                                </div>
+                                            </div>
+            
+                                            <div class="form-group">
+                                                <label for="description">Description</label>
+                                                <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+                                            </div>
+                                        </div>
+                                        <!-- /.tab-pane -->
+                                        <div class="tab-pane" id="tab_2">
+                                            <div class="form-group">
+                                                <label for="meta-title">Meta Title</label>
+                                                <input type="text" class="form-control" id="meta-title" name="meta-title">
+                                            </div>
+                                            
+                                            <div class="form-group">
+                                                <label for="meta-keywords">Meta Keywords</label>
+                                                <input type="text" class="form-control" id="meta-keywords" name="meta-keywords">
+                                            </div>
+            
+                                            <div class="form-group">
+                                                <label for="meta-description">Meta Description</label>
+                                                <textarea class="form-control" id="description" rows="3" name="meta-description"></textarea>
+                                            </div>
+                                        </div>
+                                        <!-- /.tab-pane -->
+                                    </div>
+                                    <!-- /.tab-content -->
+                                </div>
+                            </div>
+                            <div class="box-footer">
+                                <button type="submit" class="btn btn-primary pull-right">Submit</button>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
