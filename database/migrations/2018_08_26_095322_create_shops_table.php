d7a66d01bed25467d605ac9c52ca08b3a36af4dd
Abul Kalam <aazad7978@gmail.com>
2018-10-27 14:21:01 +0600

    update

--- database/migrations/2018_08_26_095322_create_shops_table.php
+++ database/migrations/2018_08_26_095322_create_shops_table.php
@@ -17,6 +17,7 @@ class CreateShopsTable extends Migration
             $table->increments('id');
             $table->string('name')->nullable();
             $table->string('slug')->unique();
+            $table->integer('country_id')->nullable();
             $table->string('status', 100)->default('draft');
             $table->string('phone_number')->nullable();
             $table->json('address')->nullable();
